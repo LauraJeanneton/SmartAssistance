7f3dabdad4fc9f4049177d583dfa5784604ebbae
LauraJeanneton <laura.jeanneton@etu.univ-amu.fr>
2020-04-18 16:15:37 +0200

    Test Deploy test return next

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ app.use(express.static('src'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-app.use(express.static(path.join(__dirname, '/build')))
+app.use(express.static(path.join(__dirname, '/build')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 });
