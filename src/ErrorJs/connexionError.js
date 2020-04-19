eb281d037ea8c0c5d5dace6d8805689febbd33db
LauraJeanneton <laura.jeanneton@etu.univ-amu.fr>
2020-04-19 15:02:31 +0200

    Test url post BDD

--- src/ErrorJs/connexionError.js
+++ src/ErrorJs/connexionError.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Appli from './App'
-import './css/Connexion.css'
+import Appli from '../App'
+import '../css/Connexion.css'
 class App extends React.Component {
     render() {
         return (
