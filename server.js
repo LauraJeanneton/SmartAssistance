8fc523fc8c81017c32ffecc82fb4b17d28b2a09a
LauraJeanneton <laura.jeanneton@etu.univ-amu.fr>
2020-04-19 14:42:49 +0200

    Test url post BDD

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const port = process.env.PORT || 5000;
 app.use(express.static(__dirname+'/src'));
 app.use(express.static('src'));
 
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.use(express.static(path.join(__dirname, '/build')));
 app.get('*', (req, res) => {
@@ -22,6 +23,8 @@ const client = new Client({
 });
 client.connect();
 
+const logger = null;
+
 app.get('/', (req, res) => {
     return res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
     next();
@@ -34,6 +37,7 @@ app.get('/connexion', function(req, res) {
 });
 
 app.post('/connexion', function(req, res) {
+    console.log(req.body.pseudo);
     client.query('SELECT login from USERS where login=$1;',['Laura'], function (err, result) {
         if (err) {
             console.log("C'est l'erreur : " + err);
@@ -95,40 +99,6 @@ function inscrFunc(){
         }
         console.log("Apres erreur");
     });
-}
-
-
-function connectFunc(){
-    haveResult = false;
-    client.query('SELECT login from USERS where login=$1;',['Laura'], function (err, result) {
-        if (err) {
-            console.log("C'est l'erreur : " + err);
-        }
-        console.log("RESULTAT 1: " +result.rows[0][0]);
-        console.log("RESULTAT 2: " +result.rows[0]);
-        console.log("RESULTAT 3: " +result.rows);
-        // console.log("RESULTAT 5: " +result.rows[0].anchor());
-        // console.log("RESULTAT 6: " +result.rows[0].valueOf());
-        // console.log("RESULTAT 7: " +result.rows.startsWith("undefined"));
-        if (result.rows[0]!=undefined) return true;
-        else return false;
-    });
-   console.log(haveResult);
-    return haveResult;
-}
-
-
-function connectFuncTest(){
-    haveResult = false;
-    client.query('SELECT login from USERS where login=$1;',['truc'], function (err, result) {
-        if (err) {
-            console.log("C'est l'erreur : " + err);
-        }
-        console.log("RESULTAT : " +result.rows[0]);
-        if (result.rows[0]!=undefined) haveResult=true;
-    });
-    console.log(haveResult);
-    return haveResult;
 }
 
 
