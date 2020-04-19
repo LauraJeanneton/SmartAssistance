109028ae19cf3a21dc640032c272e8c85101d49d
LauraJeanneton <laura.jeanneton@etu.univ-amu.fr>
2020-04-19 13:13:54 +0200

    Test insert

--- server.js
+++ server.js
@@ -34,20 +34,12 @@ app.get('/connexion', function(req, res) {
 });
 
 app.post('/connexion', function(req, res) {
-    if (connexion_users()==true) {
-        res.redirect('membre');
-    }
-    else res.redirect('connexion');
-
+    connectFunc();
+    res.redirect('membre');
 });
 
 app.post('/inscription', function(req, res) {
-    client.query('INSERT INTO USERS (login,name,city,age,password)VALUES (\'Laura\',\'Laura\',\'Marseille\',25,md5(\'test\'));', function (err, result) {
-        if (err) {
-            console.log("C'est l'erreur : "+ err);
-        }
-        console.log("Apres erreur");
-    });
+    inscrFunc();
     res.redirect('membre');
 });
 
@@ -84,55 +76,23 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 
 
 
-function connexion_bdd(){
-    const pgp = require('pg')
-
-    const cn = {
-        host: 'ec2-18-233-137-77.compute-1.amazonaws.com', // server name or IP address;
-        port: 5432,
-        database: 'd6o9rau53ul8j8',
-        user: 'mjbrgpfqdvtdps',
-        password: '04427e20360d65ea15557b5ea17c995978828f3b11232a67e517589d9a95280f',
-        ssl : true,
-        rejectUnauthorized : false,
-    };
-    const db = pgp(cn);
-    console.log("After connection");
-    return db;
-}
-
-
-function test(){
-    const pg = require('pg');
-    const pool = new pg.Pool({
-        user: 'mjbrgpfqdvtdps',
-        host: 'ec2-18-210-51-239.compute-1.amazonaws.com',
-        database: 'd6o9rau53ul8j8',
-        password: '04427e20360d65ea15557b5ea17c995978828f3b11232a67e517589d9a95280f',
-        port: 5432
-    });
-
-    pool.query("SELECT * FROM Users", (err,res) => {
-        console.log("Dans le select");
-        console.log(err,res);
-        pool.end();
+function inscrFunc(){
+    client.query('INSERT INTO USERS (login,name,city,age,password)VALUES (\'Laura\',\'Laura\',\'Marseille\',25,md5(\'test\'));', function (err, result) {
+        if (err) {
+            console.log("C'est l'erreur : "+ err);
+        }
+        console.log("Apres erreur");
     });
 }
 
 
-function connexion_users() {
-    const db = connexion_bdd();
-    db.one('SELECT * FROM Users ;', [123])
-        .then(user => {
-            console.log("Dans le then");
-            console.log(user.name); // print user name;
-        })
-        .catch(error => {
-            console.log("Dans le error");
-            console.log(error); // print the error;
-        });
-    return 0;
+function connectFunc(){
+    client.query('SELECT login from USERS where login=\'$1\');',['Laura'], function (err, result) {
+        if (err) {
+            console.log("C'est l'erreur : "+ err);
+        }
+        console.log("Apres erreur");
+    });
 }
-
 
 
