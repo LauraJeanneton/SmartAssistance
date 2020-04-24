7799696faeebd2e09e8e2cd6860b1a930febafb8
LauraJeanneton <laura.jeanneton@etu.univ-amu.fr>
2020-04-24 09:22:04 +0200

    Test connexion

--- src/Connexion.js
+++ src/Connexion.js
@@ -9,12 +9,16 @@ class Connexion extends React.Component {
             <div id={"Connect"}>
 
                 <h1> Connexion </h1>
-                <InputField label="Nom" onChange={this.props.onNameChange} onSubmit={this.props.onLogin} autoFocus />
-                <br/>
-                <label> Mot de passe :</label>
-                <input type={"text"} name={"password"} id={"password"}/>
-                <br/><br/>
-                <button onClick={this.props.onLogin}>Rejoindre</button>
+                <form action="/connexion" method="post">
+                    <InputField label="Nom" onChange={this.props.onNameChange} onSubmit={this.props.onLogin} autoFocus />
+                    <br/>
+                    <label> Mot de passe :</label>
+                    <input type={"text"} name={"password"} id={"password"}/>
+                    <br/><br/>
+                    <button onClick={this.props.onLogin}>Rejoindre</button>
+
+                </form>
+
             </div>
         </div>
     }
